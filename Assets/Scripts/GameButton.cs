1bd94e285051ce8f42661743a0957cc2f54832b3
Jaan Sangha <jaansangha10@gmail.com>
2022-03-23 20:14:58 -0400

    Switched to list instead of array

--- Assets/Scripts/GameButton.cs
+++ Assets/Scripts/GameButton.cs
@@ -12,39 +12,34 @@ public class GameButton : MonoBehaviour
     public int numOfTypes = 3;
     public int randomNumber;
     public Sprite sprite1, sprite2, sprite3;
-    public Sprite pickedSprite;
     private Vector3 mouseStart, mouseEnd;
     private float movementAngle;
-    private bool clicked;
-    // Start is called before the first frame update
+
+    public EmptySlotManager emptySlotManager;
     void Start()
     {
         minigameManager = FindObjectOfType<MinigameManager>();
+        emptySlotManager = FindObjectOfType<EmptySlotManager>();
+
+    }
+    private void Awake()
+    {
         randomNumber = Random.Range(1, numOfTypes + 1);
         SetButtonSprite();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        SetButtonSprite();
-
-    }
-    void SetButtonSprite()
+    public void SetButtonSprite()
     {
         switch(randomNumber)
         {
             case 1:
                 GetComponent<Image>().sprite = sprite1;
-                pickedSprite = sprite1;
                 break;
             case 2:
                 GetComponent<Image>().sprite = sprite2;
-                pickedSprite = sprite2;
                 break;
             case 3:
                 GetComponent<Image>().sprite = sprite3;
-                pickedSprite = sprite3;
                 break;
         }
     }
@@ -54,17 +49,12 @@ public class GameButton : MonoBehaviour
         float yDiff = mouseEnd.y - mouseStart.y;
         float xDiff = mouseEnd.x - mouseStart.x;
         movementAngle = Mathf.Atan2(yDiff, xDiff) * Mathf.Rad2Deg;
-        Debug.Log("Angle is: " + movementAngle);
-        Debug.Log("yDiff: " + yDiff);
-        Debug.Log("xDiff: " + xDiff);
-
 
         if(Mathf.Abs(movementAngle)>0.2f)
         SwapItems();
     }
     private void SwapItems()
     {
-        Debug.Log("Row: " + row + " Column: " + column);
         if(movementAngle>45 && movementAngle<135)
         {
             minigameManager.MoveUp( row,  column);
@@ -83,18 +73,26 @@ public class GameButton : MonoBehaviour
         }
     }
 
-
     public void OnPointerUp(PointerEventData eventData)
     {
         mouseEnd = eventData.position;
-        Debug.Log("end pos: " + eventData.position);
-
         CalculateAngleDifference();
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         mouseStart = eventData.position;
-        Debug.Log("start Pos" + eventData.position);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "EmptySlot")
+        {
+            if(collision.GetComponent<EmptySlotManager>())
+            {
+                row = collision.GetComponent<EmptySlotManager>().slotRow;
+                column = collision.GetComponent<EmptySlotManager>().slotColumn;
+            }
+        }
     }
 }
--- Assets/Scripts/MinigameManager.cs
+++ Assets/Scripts/MinigameManager.cs
@@ -6,8 +6,6 @@ public class MinigameManager : MonoBehaviour
 {
     [SerializeField]
     private GameButton gameButton;
-    [SerializeField]
-    private GameButton[,] buttonArray;
     private int tempSpriteNumber;
     private GameButton tempButton;
     [SerializeField]
@@ -15,24 +13,29 @@ public class MinigameManager : MonoBehaviour
     [SerializeField]
     private Transform startTransform;
     private int gridSize = 6;
+    [SerializeField]
+    private List<GameButton> buttonList;
 
     void Start()
     {
+        buttonList = new List<GameButton>();
         Vector3 tempPos = startTransform.position;
-        buttonArray = new GameButton[gridSize, gridSize];
         for (int row = 0; row < gridSize; row++)
         {
             for (int column = 0; column < gridSize; column++)
             {
-                buttonArray[row, column] = Instantiate(gameButton, tempPos, transform.rotation);
-                buttonArray[row, column].gameObject.transform.SetParent(canvas.transform);
-                buttonArray[row, column].row = row;
-                buttonArray[row, column].column = column;
+                buttonList.Add(Instantiate(gameButton, tempPos, transform.rotation));
+                buttonList[buttonList.Count - 1].gameObject.transform.SetParent(canvas.transform);
+                buttonList[buttonList.Count - 1].row = row;
+                buttonList[buttonList.Count - 1].column = column;
                 tempPos.x += 105;
+
             }
             tempPos.x = startTransform.position.x;
             tempPos.y += 106;
         }
+        //CheckForMatches();
+
     }
 
     public void MoveUp(int row, int column)
@@ -41,9 +44,16 @@ public class MinigameManager : MonoBehaviour
         {
             Debug.Log("moving up");
 
-            tempSpriteNumber = buttonArray[row + 1, column].randomNumber;
-            buttonArray[row + 1, column].randomNumber = buttonArray[row, column].randomNumber;
-            buttonArray[row, column].randomNumber = tempSpriteNumber;
+            int index = (row * 5 + row) + column;
+            tempSpriteNumber = buttonList[index+6].randomNumber;
+            buttonList[index + 6].randomNumber = buttonList[index].randomNumber;
+            buttonList[index].randomNumber = tempSpriteNumber;
+
+            buttonList[index + 6].SetButtonSprite();
+            buttonList[index].SetButtonSprite();
+
+            CheckForMatches();
+
         }
         else
             return;
@@ -54,9 +64,18 @@ public class MinigameManager : MonoBehaviour
         {
             Debug.Log("moving left");
 
-            tempSpriteNumber = buttonArray[row, column - 1].randomNumber;
-            buttonArray[row, column - 1].randomNumber = buttonArray[row, column].randomNumber;
-            buttonArray[row, column].randomNumber = tempSpriteNumber;
+            int index = (row * 5 + row) + column;
+            tempSpriteNumber = buttonList[index - 1].randomNumber;
+            buttonList[index - 1].randomNumber = buttonList[index].randomNumber;
+            buttonList[index].randomNumber = tempSpriteNumber;
+
+            buttonList[index - 1].SetButtonSprite();
+            buttonList[index].SetButtonSprite();
+
+            CheckForMatches();
+
+
+
         }
         else
             return;
@@ -67,9 +86,18 @@ public class MinigameManager : MonoBehaviour
         {
             Debug.Log("moving down");
 
-            tempSpriteNumber = buttonArray[row - 1, column].randomNumber;
-            buttonArray[row - 1, column].randomNumber = buttonArray[row, column].randomNumber;
-            buttonArray[row, column].randomNumber = tempSpriteNumber;
+            int index = (row * 5 + row) + column;
+
+            tempSpriteNumber = buttonList[index - 6].randomNumber;
+            buttonList[index - 6].randomNumber = buttonList[index].randomNumber;
+            buttonList[index].randomNumber = tempSpriteNumber;
+
+            buttonList[index - 6].SetButtonSprite();
+            buttonList[index].SetButtonSprite();
+
+            CheckForMatches();
+
+
         }
         else
             return;
@@ -80,11 +108,178 @@ public class MinigameManager : MonoBehaviour
         {
             Debug.Log("moving right");
 
-            tempSpriteNumber = buttonArray[row, column + 1].randomNumber;
-            buttonArray[row, column + 1].randomNumber = buttonArray[row, column].randomNumber;
-            buttonArray[row, column].randomNumber = tempSpriteNumber;
+            int index = (row * 5 + row) + column;
+
+            tempSpriteNumber = buttonList[index + 1].randomNumber;
+            buttonList[index + 1].randomNumber = buttonList[index].randomNumber;
+            buttonList[index].randomNumber = tempSpriteNumber;
+
+            buttonList[index + 1].SetButtonSprite();
+            buttonList[index].SetButtonSprite();
+
+            CheckForMatches();
+
+
         }
         else
             return;
+    }
+    private void Update()
+    {
+
+    }
+    private void CheckForMatches()
+    {
+        //    for (int row = 0; row < gridSize; row++)
+        //    {
+        //        for (int column = 0; column < gridSize; column++)
+        //        {
+        //            // check 2 to the left, self
+        //            if(column-2 >=0)
+        //            {
+        //                if(buttonArray[row, column - 2] != null && buttonArray[row, column - 1] != null)
+        //                {
+        //                    if ((buttonArray[row, column - 2].randomNumber == buttonArray[row, column - 1].randomNumber) && (buttonArray[row, column - 1].randomNumber == buttonArray[row, column].randomNumber))
+        //                    {
+        //                        // a match of 3
+        //                        if (buttonArray[row, column - 2].gameObject != null)
+        //                            Destroy(buttonArray[row, column - 2].gameObject);
+        //                        if (buttonArray[row, column - 1].gameObject != null)
+        //                            Destroy(buttonArray[row, column - 1].gameObject);
+        //                        if (buttonArray[row, column].gameObject != null)
+        //                            Destroy(buttonArray[row, column].gameObject);
+        //                        //buttonArray[row, column - 2].gameObject.SetActive(false);
+        //                        //buttonArray[row, column - 1].gameObject.SetActive(false);
+        //                        //buttonArray[row, column].gameObject.SetActive(false);
+        //                    }
+        //                }
+
+        //            }
+
+        //            // check one left, self, one to right
+        //            if(column-1 >=0 && column+1<=5)
+        //            {
+        //                if (buttonArray[row, column - 1] != null && buttonArray[row, column + 1] != null)
+        //                {
+        //                    if ((buttonArray[row, column - 1].randomNumber == buttonArray[row, column + 1].randomNumber) && (buttonArray[row, column + 1].randomNumber == buttonArray[row, column].randomNumber))
+        //                    {
+        //                        // a match of 3
+        //                        if (buttonArray[row, column - 1].gameObject != null)
+        //                            Destroy(buttonArray[row, column - 1].gameObject);
+        //                        if (buttonArray[row, column + 1].gameObject != null)
+        //                            Destroy(buttonArray[row, column + 1].gameObject);
+        //                        if (buttonArray[row, column].gameObject != null)
+        //                            Destroy(buttonArray[row, column].gameObject);
+
+        //                        //buttonArray[row, column - 1].gameObject.SetActive(false);
+        //                        //buttonArray[row, column + 1].gameObject.SetActive(false);
+        //                        //buttonArray[row, column].gameObject.SetActive(false);
+
+        //                    }
+        //                }
+
+        //            }
+        //            // check 2 to the right, self
+        //            if (column + 2 <= 5)
+        //            {
+        //                if (buttonArray[row, column +2] != null && buttonArray[row, column + 1] != null)
+        //                {
+        //                    if ((buttonArray[row, column + 2].randomNumber == buttonArray[row, column + 1].randomNumber) && (buttonArray[row, column + 1].randomNumber == buttonArray[row, column].randomNumber))
+        //                    {
+        //                        // a match of 3
+        //                        if (buttonArray[row, column + 2].GetComponent<GameButton>() != null)
+        //                            Destroy(buttonArray[row, column + 2].gameObject);
+        //                        if (buttonArray[row, column + 1].GetComponent<GameButton>() != null)
+        //                            Destroy(buttonArray[row, column + 1].gameObject);
+        //                        if (buttonArray[row, column].GetComponent<GameButton>() != null)
+        //                            Destroy(buttonArray[row, column].gameObject);
+
+        //                        //buttonArray[row, column + 2].gameObject.SetActive(false);
+        //                        //buttonArray[row, column + 1].gameObject.SetActive(false);
+        //                        //buttonArray[row, column].gameObject.SetActive(false);
+
+        //                    }
+        //                }
+
+        //            }
+
+        //            // check 2 up, self
+        //            if (row + 2 <= 5)
+        //            {
+        //                if (buttonArray[row +2, column] != null && buttonArray[row + 1, column] != null)
+        //                {
+        //                    if ((buttonArray[row + 2, column].randomNumber == buttonArray[row + 1, column].randomNumber) && (buttonArray[row + 1, column].randomNumber == buttonArray[row, column].randomNumber))
+        //                    {
+        //                        // a match of 3
+
+        //                        if (buttonArray[row + 2, column].GetComponent<GameButton>() != null)
+        //                            Destroy(buttonArray[row + 2, column].gameObject);
+        //                        if (buttonArray[row + 1, column].GetComponent<GameButton>() != null)
+        //                            Destroy(buttonArray[row + 1, column].gameObject);
+        //                        if (buttonArray[row, column].GetComponent<GameButton>() != null)
+        //                            Destroy(buttonArray[row, column].gameObject);
+
+        //                        //buttonArray[row + 2, column].gameObject.SetActive(false);
+        //                        //buttonArray[row + 1, column].gameObject.SetActive(false);
+        //                        //buttonArray[row, column].gameObject.SetActive(false);
+        //                    }
+        //                }
+
+        //            }
+        //            // check one up, self, one down
+        //            if (row + 1 <= 5 && row - 1 >= 0)
+        //            {
+        //                if (buttonArray[row + 1, column] != null && buttonArray[row - 1, column] != null)
+        //                {
+        //                    if ((buttonArray[row + 1, column].randomNumber == buttonArray[row - 1, column].randomNumber) && (buttonArray[row - 1, column].randomNumber == buttonArray[row, column].randomNumber))
+        //                    {
+        //                        // a match of 3
+        //                        if (buttonArray[row + 1, column].GetComponent<GameButton>() != null)
+        //                            Destroy(buttonArray[row + 1, column].gameObject);
+        //                        if (buttonArray[row - 1, column].GetComponent<GameButton>() != null)
+        //                            Destroy(buttonArray[row - 1, column].gameObject);
+        //                        if (buttonArray[row, column].GetComponent<GameButton>() != null)
+        //                            Destroy(buttonArray[row, column].gameObject);
+
+
+        //                        //buttonArray[row + 1, column].gameObject.SetActive(false);
+        //                        //buttonArray[row - 1, column].gameObject.SetActive(false);
+        //                        //buttonArray[row, column].gameObject.SetActive(false);
+        //                    }
+        //                }
+
+        //            }
+
+        //            // check 2 down, self
+        //            if (row - 2 >= 0)
+        //            {
+        //                if (buttonArray[row - 2, column] != null && buttonArray[row - 1, column] != null)
+        //                {
+        //                    if ((buttonArray[row - 2, column].randomNumber == buttonArray[row - 1, column].randomNumber) && (buttonArray[row - 1, column].randomNumber == buttonArray[row, column].randomNumber))
+        //                    {
+        //                        // a match of 3
+        //                        if (buttonArray[row - 2, column].GetComponent<GameButton>() != null)
+        //                            Destroy(buttonArray[row - 2, column].gameObject);
+        //                        if (buttonArray[row - 1, column].GetComponent<GameButton>() != null)
+        //                            Destroy(buttonArray[row - 1, column].gameObject);
+        //                        if (buttonArray[row, column].GetComponent<GameButton>() != null)
+        //                            Destroy(buttonArray[row, column].gameObject);
+
+
+        //                        //buttonArray[row - 2, column].gameObject.SetActive(false);
+        //                        //buttonArray[row - 1, column].gameObject.SetActive(false);
+        //                        //buttonArray[row, column].gameObject.SetActive(false);
+        //                    }
+        //                }
+
+
+        //            }
+        //            // 4 and 5 matches
+
+        //        }
+
+        //    }
+        //}
+
     }
 }
